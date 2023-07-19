11f1e27e3c9d83a63688e5d20f4ab8da4a664b6e
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2023-07-19 22:15:06 +0200

    feat: add basic styling for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.scss';
+import {useApi} from "./hooks/useApi";
+import List from "./components/shared/List/List";
 
-function App() {
+const App = () => {
+    const {data, error} = useApi('https://jsonplaceholder.typicode.com/albums/1/photos?limit=10');
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
     return (
         <div className="App">
             App start
+            {data && <List data={data} />}
         </div>
     );
 }

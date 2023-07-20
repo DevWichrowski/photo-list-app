f1bd4791681228aae07fabb5859bd92bba35b5ba
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2023-07-20 12:26:41 +0200

    feat: add textTruncate function

--- src/utils/funcs.ts
+++ src/utils/funcs.ts
@@ -1,3 +1,13 @@
 export const makeArray = (size: number): number[] => {
     return Array.from(new Array(size)).map((_, index) => index);
 }
+
+export const truncateText = (text: string, maxLength: number, showEllipsis = true) => {
+    if (!text || text.length <= maxLength) {
+        return text;
+    }
+
+    const truncatedText = text.substring(0, maxLength);
+    return showEllipsis ? `${truncatedText}...` : truncatedText;
+}
+
